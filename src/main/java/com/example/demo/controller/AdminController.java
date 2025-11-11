49ecaab2de05c8afa5d1f7009290b0bd9cdff529
alexxxandrdrdr <110187179+alexxxandrdrdr@users.noreply.github.com>
2025-11-11 16:35:01 +0300

    pp314 vol3

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -10,34 +10,46 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
 import java.util.*;
 
 
 @RestController
-@RequestMapping("/admin/api")
-public class AdminApiController {
+@RequestMapping("/admin")
+public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
-    private final Logger logger = LoggerFactory.getLogger(AdminApiController.class);
+    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+    private final SpringTemplateEngine templateEngine;
 
-    public AdminApiController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService, RoleService roleService, SpringTemplateEngine templateEngine) {
         this.userService = userService;
         this.roleService = roleService;
+        this.templateEngine = templateEngine;
     }
 
-    @PostMapping(value = "/addUser")
+    @GetMapping
+    public ResponseEntity<String> showAdminPanel() {
+        Context context = new Context();
+        String html = templateEngine.process("admin/pages/panel",context);
+        return ResponseEntity.ok(html);
+    }
+
+    @PostMapping(value = "/api/addUser")
     public ResponseEntity<?> createUser(@RequestBody UserEditDto userDto) {
         userService.saveUserFromDto(userDto);
         logger.info("User created successfully: {}", userDto.getEmail());
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping(value = {"/user-info/{id}"})
+    @GetMapping(value = {"api/user-info/{id}"})
     public UserEditDto getUserData(@PathVariable Long id) {
         return userService.getUserEditDtoById(id);
     }
 
-    @PatchMapping(value = "/edit-user/{id}")
+    @PatchMapping(value = "api/edit-user/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestParam String firstname, @RequestParam String lastname, @RequestParam byte age, @RequestParam String email, @RequestParam String password,@RequestParam List<Long> roles) {
         logger.info("Received update user request: {}", email);
         userService.updateUser(id, firstname, lastname, age, email, password, roles);
@@ -45,22 +57,22 @@ public class AdminApiController {
 
     }
 
-    @GetMapping(value = "/roles")
+    @GetMapping(value = "api/roles")
     public List<Role> getAllRoles() {
         return roleService.findAll();
     }
 
-    @GetMapping(value = "/users")
+    @GetMapping(value = "api/users")
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
-    @GetMapping(value = "/current-user")
+    @GetMapping(value = "api/current-user")
     public User getCurrentUser(@AuthenticationPrincipal User user) {
-        return userService.findById(user.getId()).orElse(null);
+        return user;
     }
 
-    @DeleteMapping("/delete-user/{id}")
+    @DeleteMapping("api/delete-user/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
         userService.deleteUser(id);
         return ResponseEntity.ok().build();
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,29 +2,39 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.RoleService;
+import jakarta.servlet.ServletContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     private final RoleService roleService;
+    private final SpringTemplateEngine templateEngine;
 
-    public UserController(RoleService roleService) {
+
+    public UserController(RoleService roleService, SpringTemplateEngine templateEngine) {
         this.roleService = roleService;
+        this.templateEngine = templateEngine;
     }
 
     @GetMapping
-    public String showUserPage(Model model, @AuthenticationPrincipal User user) {
-        String roles = roleService.rolesToString(user.getRoles());
-        model.addAttribute("activeRole", "ROLE_USER");
-        model.addAttribute("authUser", user);
-        model.addAttribute("roles", roles);
-        return "user/user";
+    public ResponseEntity<String> showUserPage(Model model, @AuthenticationPrincipal User user) {
+Context context = new Context();
+        context.setVariable("authUser",user);
+        context.setVariable("roles", roleService.findAll());
+        context.setVariable("activeRole", "ROLE_USER");
+        String html =templateEngine.process("user/user",context);
+
+        return ResponseEntity.ok(html);
+
     }
 }
 
